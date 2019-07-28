339377168327bb3a886d8d2ed30009f4edf8a55e
temur <temur@DESKTOP-O3F9GFL>
2019-07-28 21:40:31 +0530

    done

--- src/main/java/main/Question2.java
+++ src/main/java/main/Question2.java
@@ -20,8 +20,9 @@ public static void main(String[] args) {
 
 }
 public List<Integer> listWithDuplicates(List<Integer> number){
-	List <Integer> L1 = null;
+	List <Integer> L1 = number;
 	//write your code here !! change the L1 to required value
+	
 	return L1 ;
 }
 }
